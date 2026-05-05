c42cb33c6faed890160ceb6d37884e75f6839f38
Lovro Vrcek <vrcek.lovro@gmail.com>
2026-05-05 14:23:22 +0800

    Add ncRNA path

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -11,7 +11,8 @@ PATHS = {
     'savedir':             _scratch,
     'og2_dataset':         _data / 'opengenome2_subset/preprocessed_12M_uppercase',
     'wiki_dataset':        _data / 'wikipedia_packed',
-    'ensembl_dataset': _scratch / 'ensembl_cdna_chunks_4096',
+    'ensembl_dataset':     _scratch / 'ensembl_cdna_chunks_4096',
+    'ncrna_dataset':       _scratch / 'ensembl_ncRNA_3072',
     'cache_dir':           _data,
 }
 
