1a3f606001c294382fc93752e92b7ddc426ef2ac
Lovro Vrcek <vrcek.lovro@gmail.com>
2026-05-14 15:11:32 +0800

    Forgot to include the paths.py

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -8,12 +8,14 @@ PATHS = {
     'dna_bpe_tokenizer':   _scratch / 'bpe_dna_tokenizer',
     'dna_lcp_tokenizer':   _scratch / 'lcp_dna_tokenizer',
     'checkpoints':         _scratch / 'checkpoints',
+    'analyze':             _scratch / 'analyze',
     'savedir':             _scratch,
     'og2_dataset':         _data / 'opengenome2_subset/preprocessed_12M_uppercase',
     'wiki_dataset':        _data / 'wikipedia_packed',
     'ensembl_dataset':     _scratch / 'ensembl_cdna_chunks_4096',
     'ncrna_dataset':       _scratch / 'ensembl_ncRNA_3072',
     'cache_dir':           _data,
+    'username':            'user',
 }
 
 try:
